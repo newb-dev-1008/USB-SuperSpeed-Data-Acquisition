03c18a48109a86c584bd3496b19cfe49c3f8ebe6
Yash Umale <yashumale223@gmail.com>
2022-03-10 15:17:32 +0530

    Initial Commit

---
+++ Phase 2/Technology Independent Coding Styles/dff_rising.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use IEEE.std_logic_1164.all;
+    
+entity dff is
+port (data, clk : in std_logic;
+    q : out std_logic);
+end dff;
+
+architecture behav of dff is
+begin
+process (clk) begin
+    if (clk'event and clk = '1') then
+        q <= data;
+    end if;
+end process;
+end behav;
