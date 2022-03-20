459b9bda62db78763ffdafa977ceff7c427ca8c5
Yash Umale <yashumale223@gmail.com>
2022-03-20 13:21:39 +0530

    32 bit counter

--- Phase 2/Technology Independent Coding Styles/Libero SoC projects/Counter32_8BitCounters/hdl/counter.vhd
+++ Phase 2/Technology Independent Coding Styles/Libero SoC projects/Counter32_8BitCounters/hdl/counter.vhd
@@ -12,28 +12,67 @@
 -- <Description here>
 --
 -- Targeted device: <Family::ProASIC3> <Die::M1A3P1000> <Package::208 PQFP>
--- Author: <Name>
+-- Author: Yash Umale
 --
 --------------------------------------------------------------------------------
 
 library IEEE;
 
 use IEEE.std_logic_1164.all;
+use IEEE.math_real.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_arith.all;
+use IEEE.numeric_std.all;
 
 entity counter is
 port (
-    --<port_name> : <direction> <type>;
-	port_name1 : IN  std_logic; -- example
-    port_name2 : OUT std_logic_vector(1 downto 0)  -- example
-    --<other_ports>;
-);
+	clk, rst : IN  std_logic;
+    q : OUT std_logic_vector(7 downto 0));
 end counter;
+
 architecture architecture_counter of counter is
-   -- signal, component etc. declarations
-	signal signal_name1 : std_logic; -- example
-	signal signal_name2 : std_logic_vector(1 downto 0) ; -- example
+
+    -- Function to define an 8-bit counter
+    function counter8(prev : unsigned(7 downto 0); rst : integer := 0) return integer is
+        variable outp : unsigned(7 downto 0) := to_unsigned(0, 8);
+        begin 
+            if (rst = '1') then
+                outp <= to_unsigned(0, 8);
+            else
+                outp <= prev + '1';
+            end if;
+            return outp;
+        end
+
+    -- signal, component etc. declarations
+	signal count_val : std_logic_vector(31 downto 0);
+    variable cnt : integer := '0';
 
 begin
+    process(clk) begin
+        if (clk'event and rst = '1') then
+            count_val <= to_unsigned(0, 32);
+            cnt <= '0';
+        
+        elsif (clk'event and clk = '1') then
+            cnt <= cnt + '1';
 
-   -- architecture body
+            if (cnt mod (2 ** 8) = '0') then
+                if (cnt mod (2 ** 16) = '0') then
+                    if (cnt mod (2 ** 24) = '0') then
+                        count_val[31:24] <= counter8(prev => count_val[31:24]);
+                        count_val[23:0] <= to_unsigned(0, 24);
+                    else
+                        count_val[23:16] <= count8(prev => count_val[23:16]);
+                        count_val[15:0] <= to_unsigned(0, 16);
+                    end if
+                else
+                    count_val[15:8] <= count8(prev => count_val[15:8]);
+                    count_val[7:0] <= to_unsigned(0, 8);
+            
+                end if
+            else 
+                count_val[7:0] <= count8(prev => count_val[7:0]);
+            end if
+            
 end architecture_counter;
